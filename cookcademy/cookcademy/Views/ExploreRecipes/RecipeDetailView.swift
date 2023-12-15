a4e28d9a5b32f7db6887fb148f5a8cc1d8a9e835
Pi <telfortps@outlook.com>
2023-12-14 20:14:27 -0600

    Updated views to be able to edit ingredients

--- cookcademy/cookcademy/Views/ExploreRecipes/RecipeDetailView.swift
+++ cookcademy/cookcademy/Views/ExploreRecipes/RecipeDetailView.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct RecipeDetailView: View {
-    let recipe: Recipe
+    @Binding var recipe: Recipe
 
     private let listBackgroundColor = AppColor.background
     private let listTextColor = AppColor.foreground
+
+    @State private var isPresenting = false
 
     var body: some View {
         VStack {
@@ -47,6 +49,29 @@ struct RecipeDetailView: View {
             }
         }
         .navigationTitle(recipe.mainInformation.name)
+        .toolbar {
+            ToolbarItem {
+                HStack {
+                    Button("Edit") {
+                        isPresenting = true
+                    }
+                }
+            }
+            ToolbarItem(placement: .navigationBarLeading) { Text("") }
+        }
+        .sheet(isPresented: $isPresenting) {
+            NavigationView {
+                ModifyRecipeView(recipe: $recipe)
+                    .toolbar {
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Save") {
+                                isPresenting = false
+                            }
+                        }
+                    }
+                    .navigationTitle("Edit Recipe")
+            }
+        }
     }
 }
 
@@ -54,7 +79,7 @@ struct RecipeDetailView_Previews: PreviewProvider {
     @State static var recipe = Recipe.testRecipes[0]
     static var previews: some View {
         NavigationView {
-            RecipeDetailView(recipe: recipe)
+            RecipeDetailView(recipe: $recipe)
         }
     }
 }

396bbdaeed76141fd838275585e7bf8a53ed2b43
Pi <telfortps@outlook.com>
2023-12-17 13:21:21 -0600

    Created a new project to test random users api

---
+++ RandomUsers/RandomUsers/UsersView.swift
@@ -0,0 +1,30 @@
+//
+//  UsersView.swift
+//  RandomUsers
+//  Created by pitel on 12/17/23.
+//
+
+import SwiftUI
+
+struct UsersView: View {
+    @StateObject var userData = UserData()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Raw JSON Data:")
+                ScrollView {
+                    Text(userData.users)
+                }
+            }
+            .padding()
+            .navigationTitle("Random Users")
+        }
+    }
+}
+
+struct UsersView_Previews: PreviewProvider {
+    static var previews: some View {
+        UsersView()
+    }
+}

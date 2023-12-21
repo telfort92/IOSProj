1495034a98dcb55fc42288b69a8325755e515fed
Pi <telfortps@outlook.com>
2023-12-21 15:52:35 -0600

    Enhanced the social cademy project

---
+++ Socialcademy/Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,22 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by pitel on 12/21/23.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct SocialcademyApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            PostsList()
+        }
+    }
+}
---
+++ Socialcademy/Socialcademy/View Models/PostsViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  PostsViewModel.swift
+//  Socialcademy
+//
+//  Created by pitel on 12/21/23.
+//
+
+import Foundation
+
+@MainActor
+class PostsViewModel: ObservableObject {
+    @Published var posts: Loadable<[Post]> = .loading
+    
+    private let postsRepository: PostsRepositoryProtocol
+    
+    init(postsRepository: PostsRepositoryProtocol = PostsRepository()) {
+        self.postsRepository = postsRepository
+    }
+    
+    func fetchPosts() {
+        Task {
+            do {
+                posts = .loaded(try await postsRepository.fetchPosts())
+            } catch {
+                print("[PostsViewModel] Cannot fetch posts: \(error)")
+                posts = .error(error)
+            }
+        }
+    }
+    
+    func makeCreateAction() -> NewPostForm.CreateAction {
+        return { [weak self] post in
+            try await self?.postsRepository.create(post)
+            self?.posts.value?.insert(post, at: 0)
+        }
+    }
+}

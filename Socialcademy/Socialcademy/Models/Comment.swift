747174977107440b22f010dd01d25cb6cd4e8ef3
Pi <telfortps@outlook.com>
2023-12-31 15:31:15 -0600

    Enhance the project to add comments

---
+++ Socialcademy/Socialcademy/Models/Comment.swift
@@ -0,0 +1,19 @@
+//
+//  Comment.swift
+//  Socialcademy
+//
+//  Created by pitel on 12/30/23.
+//
+
+import Foundation
+
+struct Comment: Identifiable, Equatable, Codable {
+    var content: String
+    var author: User
+    var timestamp = Date()
+    var id = UUID()
+}
+
+extension Comment {
+    static let testComment = Comment(content: "Lorem ipsum dolor set amet.", author: User.testUser)
+}

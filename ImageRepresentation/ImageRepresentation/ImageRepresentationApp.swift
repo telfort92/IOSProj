58fd7418524c570be21668c973c74734eab70e27
Pi <telfortps@outlook.com>
2023-12-31 16:16:28 -0600

    This project is an example how to present mages

---
+++ ImageRepresentation/ImageRepresentation/ImageRepresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRepresentationApp.swift
+//  ImageRepresentation
+//
+//  Created by pitel on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageRepresentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
